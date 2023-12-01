5924ad53e9cae6bf245edf8a613e266202b91118
Kush Shah <kushshah@Kushs-MacBook-Pro.local>
2023-11-30 22:45:28 -0700

    fix space between DMN logo and breadcrumbs

--- src/pages/ArtistHomepage.tsx
+++ src/pages/ArtistHomepage.tsx
@@ -38,7 +38,7 @@ const ArtistHomepage: React.FC = () => {
         display: "flex",
         flexDirection: "column",
         justifyContent: "space-between",
-        paddingTop: "15px",
+        paddingTop: "25px",
       }}
     >
       {/* Top Bar */}
